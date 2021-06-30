3c6e5091de80e7f0281fe465591233e57d05e2bc
Veronica <petersonv2913@gmail.com>
2021-06-30 16:06:50 -0400

    added some fake info for database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+#prevention of CSRF 
+protect_from_forgery with: :exception
 #add session cookie here with form_authenticity_token and set a before_action to put a CSRF token in the cookie
+
+
 
 end
