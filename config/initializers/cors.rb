e204d09bc907bea3980755fd127b2d986ed9e5a6
Veronica <petersonv2913@gmail.com>
2021-06-30 12:45:00 -0400

    uncommented bcypt gem for password digest

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
         #change wildcard origin when it is known
       origins '*'
+      #do i need more request options here?
       resource '*', headers: :any, methods: [:get, :post]
     end
   end
